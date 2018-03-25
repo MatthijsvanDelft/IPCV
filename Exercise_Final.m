afd598dd27626a15c7f9306ef467adee56d52a53
gijsje789 <gijsjaapdevries@hotmail.com>
2018-03-25 15:14:05 +0200

    Load camera calibration matrices

--- Exercise_Final.m
+++ Exercise_Final.m
@@ -1,7 +1,15 @@
 %% Final assignment
+% Clear the workspace and close all figures.
 clear variables
 close all
 
+%% Retrieve calibration information.
+% Load the camera parameters.
+load('cameraParams.mat');
+% Load the estimation errors during the camera calibration.
+load('estimationErrors.mat');
+
+%% Show the video
 video = VideoReader('MAH01462.MP4');
 videoWidth = video.Width;
 videoHeight = video.Height;
