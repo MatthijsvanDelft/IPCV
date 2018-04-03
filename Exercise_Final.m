01764dd6ea41701d2325cf9ef1ab767986fedc0d
gijsje789 <33655872+gijsje789@users.noreply.github.com>
2018-04-03 14:04:07 +0200

    Version 0.1 (#1)
    * Camera calibration script
    
    Generated by MATLAB and is not necessary to run multiple times.
    
    * MATLAB calibration output
    
    Matrices generated by the Camera Calibration App.
    
    * MATLAB camera calibration session
    
    Not sure what this is, saved just to be sure. It is the CameraCalibration App's session.
    
    * Load camera calibration matrices
    
    * Ignore all .asv files.
    
    I think these files are temporary files made by MATLAB or windows.
    
    * Extract coordinates of buoy in first frame
    
    Only calls getpts in first frame. Continues asking for points if either too many points are chosen (i.e. > 1) or if no points are chosen (i.e. array is empty).
    
    * Draws the search grid over the image
    
    * Limiting to the video's FPS instead of 30
    
    * UT functions as supplied by Ferdi van der Heijden
    
    * Plots the camera distortion
    
    * Corrected the lens distortion plot
    
    * Undistort the video and show it
    
    Somehow, if there is no pause then it doesn't show the frame even if capped at 25 fps. Therefore, constant pause at the end.
    
    * Estimate optical flow and plot
    
    Note: plotting the optical flow is rather slow as in 2+ seconds per frame
    
    * Drawnow limitrate to draw at least at 20fps
    
    * WMV version of mp4 video
    
    * Estimate optical flow and plot
    
    * Revert "WMV version of mp4 video"
    
    This reverts commit 455659ad6c85302628172c49c66b5c2aa1d7b84c.
    
    * Git ignore
    
    * Video stabilisation RT
    
    * Similarity instead of affine, removed redundant code
    
    * Cutout of search area
    
    * Polar scatter of found flow vectors
    
    * Changed optical flow to LKDOG
    
    * Subfunction for drawing the searchgird
    
    * Showing of warped image, zoomed image, and erode/dilate combination
    
    * Extract feature information, plot labels
    
    * Find eccentric blobs, determine distance to center of search grid, adjust searchgrid accoding to closest to center
    
    * Most circular and closest to center
    
    * Combining eccentricity and a Gaussian probability based on the distance from the center.
    
    * Solved stabilization issue where transformed frame was not used in a cumulative way.

--- Exercise_Final.m
+++ Exercise_Final.m
@@ -1,14 +1,167 @@
 %% Final assignment
+% Clear the workspace and close all figures.
 clear variables
 close all
 
-video = VideoReader('MAH01462.MP4');
+%% Retrieve calibration information.
+% Load the camera parameters.
+load('cameraParams.mat');
+% Load the estimation errors during the camera calibration.
+load('estimationErrors.mat');
+
+% Parameters
+widthSearchArea = 200; % In pixels.
+heightSearchArea = 200; % In pixels.
+
+%% Show the video
+% Read in the video and get its width and height.
+video = VideoReader('MAH01462.wmv');
 videoWidth = video.Width;
 videoHeight = video.Height;
+videoFPS = video.FrameRate;
+
+% Plot the lens distortion
+ut_plot_lens_distortion(cameraParams, [videoHeight videoWidth]);
+
+% Initialise a frame counter.
+currentFrame = 0;
+% Initialise initial buoy coordinates.
+xBuoy = [];
+yBuoy = [];
+
+ptThresh = 0.1;
+
+% Create optical flow object using Lucas-Kanade
+flowObj = opticalFlowLKDoG( 'NoiseThreshold', 0.0012, 'NumFrames', 3,...
+                            'ImageFilterSigma', 3.5, ...
+                            'GradientFilterSigma', 4.5);
+% flowObj = opticalFlowLK( 'NoiseThreshold', 0.0144);
+%Loop through the video.
+flowPhaseMag = figure;
+videoFigure = figure;
+
+% Probability gaussian.
+sigma = 30; %Trial and error.
+h = fspecial('gaussian', [widthSearchArea heightSearchArea], sigma);
+normH = h - min(h(:));
+h = normH ./ max(normH(:));
+
+% Blob analyser.
+minBlobArea = 6;
+maxBlobArea = 12;
+blobInfo = vision.BlobAnalysis('LabelMatrixOutputPort', true, 'EccentricityOutputPort', true, 'MinimumBlobArea', minBlobArea, 'MaximumBlobArea', maxBlobArea);
 
 while hasFrame(video)
+    tic;
+    %figure(videoFigure)
+    % Video has a new frame, thus increment currentFrame.
+    currentFrame = currentFrame + 1;
     frame = readFrame(video, 'native');
-    imshow(frame)
-end
+    prevVect = widthSearchArea;
+    closestToCenter = [widthSearchArea/2 heightSearchArea/2];
+    
+    % Fix lens distortion.
+    [frameUndistorted,~] = undistortImage(frame,cameraParams);
+    
+    if currentFrame == 1
+        % Only extract the coordinates of the buoy in the first frame.
+        % GCF is the MATLAB key to the current figure.
+        imshow(frame)
+        while (size(xBuoy, 1) > 1 || size(yBuoy, 1) > 1) || (isempty(xBuoy) || isempty(yBuoy))
+            uiwait(msgbox({'Please pick one point.';...
+                           'A shift-, right-, or double-click adds a final point and ends the selection.';...
+                           'Pressing Return or Enter ends the selection without adding a final point. ';...
+                           'Pressing Backspace or Delete removes the previously selected point.'}, 'Attention!', 'help'));
+            [xBuoy, yBuoy] = getpts(gcf);
+        end
+        framePrev = frameUndistorted;
+    end
+    
+    if currentFrame >=2
+        
+        % Keypoint detection.
+        pointsCur = detectFASTFeatures(rgb2gray(frameUndistorted), 'MinContrast', ptThresh);
+        pointsPrev = detectFASTFeatures(rgb2gray(framePrev), 'MinContrast', ptThresh);
+        
+        % Feature extraction.
+        [featuresCur, pointsCur] = extractFeatures(rgb2gray(frameUndistorted), pointsCur);
+        [featuresPrev, pointsPrev] = extractFeatures(rgb2gray(framePrev), pointsPrev);
+        
+        % Keypoint matching.
+        indexPairs = matchFeatures(featuresPrev, featuresCur);
+        pointsPrev = pointsPrev(indexPairs(:,1), :);
+        pointsCur = pointsCur(indexPairs(:,2), :);
+        
+%         showMatchedFeatures(framePrev, frameUndistorted, pointsPrev, pointsCur);
+        
+        % Stabilization transformation.
+        [tform, pointsCurm, pointsPrevm] = estimateGeometricTransform(pointsCur, pointsPrev, 'similarity');
+        frameUndistortedWarped = imwarp(frameUndistorted, tform, 'OutputView', imref2d(size(frameUndistorted)));
+        
+        % ROI.
+        frameRef = [xBuoy - 0.5*widthSearchArea yBuoy - 0.5*heightSearchArea];
+        frameCutout = frameUndistortedWarped(yBuoy - 0.5*heightSearchArea : yBuoy + 0.5*heightSearchArea,...
+                                             xBuoy - 0.5*widthSearchArea : xBuoy + 0.5*widthSearchArea,...
+                                             :);
+        
+        % Visualization.
+        subplot(2,2,1)
+        imshow(frameUndistortedWarped);
+        hold on
+        drawSearchGrid(xBuoy, yBuoy, widthSearchArea, heightSearchArea);
+        hold off
+        title('Original image with camera stabilisation');
+        
+        subplot(2,2,4)
 
-%getpts();
+        % Morphological operations.
+        Thres = adaptthresh(rgb2gray(frameCutout), 0.20);
+        bin = imclearborder(imbinarize(rgb2gray(frameCutout), Thres));
+        erod = imerode(bin, strel('disk', 1));
+        dila = imdilate(erod, strel('disk', 1));        
+        %imshow((dila));
+        
+        % Blob analysis.
+        [area, centroid, bbox, eccentricity, labeled] = blobInfo.step(dila);
+        imshow(label2rgb(labeled));
+        hold on;
+        plot(widthSearchArea/2, heightSearchArea/2, 'r+');
+        
+        % Calculate probability.
+        numberBlobs = size(eccentricity, 1);
+        blobProb = zeros(numberBlobs,1);
+        
+        for b = 1:numberBlobs
+            blobProb(b,:) = (1-eccentricity(b,:))*h(round(centroid(b,1)), round(centroid(b,2)));                        
+        end
+        
+        thresProb = 0.005;
+        [M,I] = max(blobProb(:));
+        if (M >= thresProb)
+            xBuoy = round(centroid(I,1)) + frameRef(1);
+            yBuoy = round(centroid(I,2)) + frameRef(2);                
+        end
+        
+        hold off;
+        title('Thresholded, eroded, dilated and labeled searchgrid');
+        
+        subplot(2,2,3)
+        %polarscatter(-flow.Orientation(:), flow.Magnitude(:), '.');
+        imshow(frameCutout);
+        
+        title('Zoomed searchgrid');
+        
+        subplot(2,2,2)
+        imshow((dila));%imclearborder(imbinarize(rgb2gray(frameCutout), Thres)));
+        %imshow(frameUndistortedWarped)
+        %figure(flowPhaseMag);        
+        
+        title('Thresholded, eroded and dilated searchgrid');
+    
+        framePrev = frameUndistortedWarped;        
+    end
+    
+    T = toc
+    
+    drawnow limitrate
+end
